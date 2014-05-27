77b94898acc660744a82ab6ea946095eb7a1974e
= <leogodin217@gmail.com>
2014-05-27 14:49:07 -0700

    Enable login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ SampleApp::Application.routes.draw do
   match '/signup',  to: 'users#new',            via: 'get'
   root 'static_pages#home'
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,13 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
   it { should be_valid }
   describe "when name is not present" do
@@ -97,4 +103,5 @@ describe "when email format is invalid" do
       specify { expect(user_for_invalid_password).to be_false }
     end
   end
+
 end
